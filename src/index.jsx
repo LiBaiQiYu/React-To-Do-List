6622a377f475484be5ff7d0cd87855002cbadbd2
BaiYuChen <625071316@qq.com>
2024-01-08 18:22:39 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { Button, Input, Radio, Checkbox } from 'antd';
 
 function Top(props) {
-  const state = props.props;
+  const { inputValue, setInputValue, addList } = props.props;
   return (
     <div>
-      <Input value={state.inputValue} onChange={(e) => state.setInputValue(e.target.value)} placeholder="请输入事项" style={{ width: 400 }} />
-      <Button type="primary" onClick={state.addList}>添加待办</Button>
+      <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="请输入事项" style={{ width: 400 }} />
+      <Button type="primary" onClick={addList}>添加待办</Button>
       <br></br>
     </div>
   )
@@ -15,22 +15,18 @@ function Top(props) {
 
 function TodoList(props) {
   const [value, setValue] = useState(1);
-  const state = props.props;
+  const { listArr, setListArr } = props.props;
   const onChange = (e) => {
-    console.log('radio checked', e.target.value);
-    console.log(state.listArr);
     setValue(e.target.value);
   };
-  const checkChange = (key,e) => {
-    console.log(e);
-    console.log(state);
-    let newArr = state.listArr.slice();
-    newArr.map(item => {
+  const checkChange = (key, e) => {
+    let newArr = listArr.slice();
+    newArr.forEach(item => {
       if (item.key === key) {
         item.checked = !item.checked
       }
     })
-    state.setListArr(newArr);
+    setListArr(newArr);
   }
   return (
     <div>
@@ -40,8 +36,7 @@ function TodoList(props) {
         <Radio value={3}>未完成</Radio>
       </Radio.Group>
       <ul>
-        {/* {state.listArr} */}
-        {state.listArr.filter(item => {
+        {listArr.filter(item => {
           if (value === 1) {
             return item
           } else if (value === 2) {
@@ -49,18 +44,18 @@ function TodoList(props) {
           } else {
             return !item.checked
           }
-        }).map(item => <Item props={item} key={item.key} checkChange={(e) => checkChange(item.key,e)} />)}
+        }).map(item => <Item props={item} key={item.key} checkChange={(e) => checkChange(item.key, e)} />)}
       </ul>
     </div>
   )
 }
 
 function Item(props) {
-  const state = props.props;
+  const { checked, inputValue, deleteItem } = props.props;
   return (<li style={{ width: '300px', display: 'flex', justifyContent: 'space-between' }}>
-    <Checkbox onClick={props.checkChange} checked={state.checked}></Checkbox>
-    <div style={{ flex: '1', padding: '0 10px' }}>{state.inputValue}</div>
-    <Button onClick={() => state.deleteItem(state.inputValue)} type="dashed" danger >
+    <Checkbox onClick={props.checkChange} checked={checked}></Checkbox>
+    <div style={{ flex: '1', padding: '0 10px' }}>{inputValue}</div>
+    <Button onClick={() => deleteItem(inputValue)} type="dashed" danger >
       删除
     </Button>
   </li>)
@@ -78,9 +73,7 @@ function Main() {
   }
 
   const deleteItem = (itemKey) => {
-    console.log(itemKey)
     const newArr = stateRef.value.filter((val) => val.key !== itemKey)
-    console.log(newArr)
     setListArr(newArr)
   }
 
