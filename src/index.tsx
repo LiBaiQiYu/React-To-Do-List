ce06aaf6eaaadabbf03fd9f9d678bdc8b4ab2019
BaiYuChen <625071316@qq.com>
2024-01-15 20:03:09 +0800

    更换TS，Antd更换为MUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,24 @@
 import React, { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { Button, Input, Radio, Checkbox } from 'antd';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { TextField, Button,Checkbox,RadioGroup, FormControlLabel, Radio } from '@mui/material';
 
 function Top(props) {
   const { inputValue, setInputValue, addList } = props.state;
   return (
     <div>
-      <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="请输入事项" style={{ width: 400 }} />
-      <Button type="primary" onClick={addList}>添加待办</Button>
+      <TextField id="outlined-basic" label="Input Items" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="input item" variant="outlined" />
+      <Button variant="contained" onClick={addList}>ADD ITEM</Button>
       <br></br>
     </div>
   )
 }
 
 function TodoList(props) {
-  const [value, setValue] = useState(1);
+  const [value, setValue] = useState("1");
   const { listArr, setListArr } = props.state;
   const onChange = (e) => {
     setValue(e.target.value);
@@ -30,9 +34,9 @@ function TodoList(props) {
   }
 
   let newItem = listArr.filter(item => {
-    if (value === 1) {
+    if (value === "1") {
       return item
-    } else if (value === 2) {
+    } else if (value === "2") {
       return item.checked
     } else {
       return !item.checked
@@ -40,11 +44,16 @@ function TodoList(props) {
   }).map(i => <Item state={i} key={i.key} checkChange={(e) => checkChange(i.key, e)} />)
   return (
     <div>
-      <Radio.Group onChange={onChange} value={value}>
-        <Radio value={1}>全部</Radio>
-        <Radio value={2}>已完成</Radio>
-        <Radio value={3}>未完成</Radio>
-      </Radio.Group>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+        onChange={onChange} value={value}
+      >
+        <FormControlLabel value={"1"} control={<Radio />} label="全部" />
+        <FormControlLabel value={"2"} control={<Radio />} label="已完成" />
+        <FormControlLabel value={"3"} control={<Radio />} label="未完成" />
+      </RadioGroup>
       <ul>
         {newItem}
       </ul>
@@ -55,33 +64,39 @@ function TodoList(props) {
 function Item(props) {
   const { checked, inputValue, deleteItem } = props.state;
   const { checkChange } = props;
+  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
   return (<li style={{ width: '300px', display: 'flex', justifyContent: 'space-between', marginBottom: '10px' }}>
-    <Checkbox onClick={checkChange} checked={checked}></Checkbox>
+    <Checkbox {...label} onClick={checkChange} checked={checked} />
     <div style={{ flex: '1', padding: '0 10px' }}>{inputValue}</div>
-    <Button onClick={() => deleteItem(inputValue)} type="dashed" danger >
-      删除
-    </Button>
+    <Button variant="contained" color="error" onClick={() => deleteItem(inputValue)} >DEL ITEM</Button>
   </li>)
 }
 
 function Main() {
   const [inputValue, setInputValue] = useState('');
-  const [listArr, setListArr] = useState([]);
-  const stateRef = useRef(null);
+  const [listArr, setListArr] = useState<Item[]>([]);
+  const stateRef = useRef<Item[]>([]);
 
-  const addList = (e) => {
+  interface Item {
+    inputValue: string,
+    deleteItem: any,
+    key: string,
+    checked: boolean,
+  }
+
+  const addList = () => {
     if (inputValue === '') return;
     setListArr([...listArr, itemProps])
     setInputValue('')
   }
 
-  const deleteItem = (itemKey) => {
-    const newArr = stateRef.value.filter((val) => val.key !== itemKey)
+  const deleteItem = (itemKey: string) => {
+    const newArr = stateRef.current.filter((val) => val.key !== itemKey)
     setListArr(newArr)
   }
 
   useEffect(() => {
-    stateRef.value = listArr
+    stateRef.current = listArr
   })
 
   const topProps = {
@@ -109,8 +124,12 @@ function Main() {
     </div>
   )
 }
+const dom = document.getElementById("root");
+if (dom !== null) {
+  const root = ReactDOM.createRoot(dom);
+  root.render(<Main />);
+}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Main />);
+
 
 
